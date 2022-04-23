58e176f08ada85e33a27629a97d650eaaf9512c2
Stefano Consonni <stefanoconsonni@ymail.com>
2022-04-23 22:20:36 +0200

    add campground delete route and form

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ app.put("/campgrounds/:id", async (req, res) => {
 	res.redirect(`/campgrounds/${campground._id}`);
 });
 
+app.delete("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id);
+	res.redirect("/campgrounds");
+});
+
 app.listen(port, () => {
 	console.log("Serving on port 3000!");
 });
