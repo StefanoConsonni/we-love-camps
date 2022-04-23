926ea93b12eadc9eb66e9672f4433cf9a43cd1fa
Stefano Consonni <stefanoconsonni@ymail.com>
2022-04-23 19:26:16 +0200

    add campground edit and update features

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const port = 3000;
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/we-love-camps");
@@ -16,6 +17,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
 	res.render("home");
@@ -39,6 +41,17 @@ app.post("/campgrounds", async (req, res) => {
 app.get("/campgrounds/:id", async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(port, () => {
