8ba1e2b25240ea973381313a980bdb18949cea6a
Stefano Consonni <stefanoconsonni@ymail.com>
2022-04-23 18:09:00 +0200

    add campground form and create

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
@@ -22,6 +24,16 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+	res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
