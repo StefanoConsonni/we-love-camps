8b61d3aea289a1c6ffe48de3e900591e11d17284
Stefano Consonni <stefanoconsonni@ymail.com>
2022-04-25 18:55:00 +0200

    fix problem with sass compiling + add sass middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const port = 3000;
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const sassMiddleware = require("node-sass-middleware");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -20,6 +21,15 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(
+	sassMiddleware({
+		src: path.join(__dirname, "sass"),
+		dest: path.join(__dirname, "public/css"),
+		indentedSyntax: false, // true = .sass and false = .scss
+		sourceMap: true,
+	})
+);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
 	res.render("home");
