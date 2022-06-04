077aa5ef9468f8ec22ae797fef49e56e35bd194d
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-05 00:30:20 +0200

    add Joi Schema Validation in app.js

--- app.js
+++ app.js
@@ -53,7 +53,20 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
 	"/campgrounds",
 	catchAsync(async (req, res, next) => {
-		if (!req.body.campground) throw new ExpressError("Invalid Campground Data!", 400);
+		// if (!req.body.campground) throw new ExpressError("Invalid Campground Data!", 400);
+		const campgroundSchema = Joi.object({
+			campground: Joi.object({
+				title: Joi.string().required(),
+				price: Joi.number().required().min(0),
+			}).required(),
+		});
+		const { error } = campgroundSchema.validate(req.body);
+		if (error) {
+			const msg = error.details.map((el) => el.message).join(",");
+			throw new ExpressError(msg, 400);
+		}
+
+		console.log(result);
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
