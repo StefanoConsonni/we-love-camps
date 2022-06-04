720cb4e5170fbc05a12b4d1f2f4c139196caff06
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-04 19:04:29 +0200

    add ExpressError class

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const sassMiddleware = require("node-sass-middleware");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
@@ -51,6 +52,7 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
 	"/campgrounds",
 	catchAsync(async (req, res, next) => {
+		if (!req.body.campground) throw new ExpressError("Invalid Campground Data!", 400);
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -91,8 +93,13 @@ app.delete(
 	})
 );
 
+app.all("*", (req, res, next) => {
+	next(new ExpressError("Page Not Found", 404));
+});
+
 app.use((err, req, res, next) => {
-	res.send("Oh Boy, something went wrong!");
+	const { statusCode = 500, message = "Something went wrong!" } = err;
+	res.status(statusCode).send(message);
 });
 
 app.listen(port, () => {
