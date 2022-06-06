a12ce164d900d664b645163e0234dc15383b4fba
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-06 16:50:27 +0200

    add review validation with joi library

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const Joi = require("joi");
-const { campgroundSchema } = require("./schemas");
+const { campgroundSchema, reviewSchema } = require("./schemas");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const sassMiddleware = require("node-sass-middleware");
@@ -38,6 +38,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const validateCampground = (req, res, next) => {
 	const { error } = campgroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(",");
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
+const validateReview = (req, res, next) => {
+	const { error } = reviewSchema.validate(req.body);
 	if (error) {
 		const msg = error.details.map((el) => el.message).join(",");
 		throw new ExpressError(msg, 400);
@@ -108,6 +118,7 @@ app.delete(
 
 app.post(
 	"/campgrounds/:id/reviews",
+	validateReview,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		const review = new Review(req.body.review);
