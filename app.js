ebef1f602da8ab7f6ca1d96ecb0103a16951e3ac
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-07 21:53:34 +0200

    create delete route for reviews

--- app.js
+++ app.js
@@ -129,6 +129,16 @@ app.post(
 	})
 );
 
+app.delete(
+	"/campgrounds/:id/reviews/:reviewId",
+	catchAsync(async (req, res) => {
+		const { id, reviewId } = req.params;
+		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+		await Review.findByIdAndDelete(reviewId);
+		res.redirect(`/campgrounds/${id}`);
+	})
+);
+
 app.all("*", (req, res, next) => {
 	next(new ExpressError("Page Not Found", 404));
 });
