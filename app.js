74451e35be5aaf7d8a0a720c8d49de8281dbe941
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-12 18:50:34 +0200

    set up passport.js and create user register route

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const port = 3000;
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
@@ -8,9 +7,13 @@ const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const sassMiddleware = require("node-sass-middleware");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
+const usersRoutes = require("./routes/users");
 
 mongoose.connect("mongodb://localhost:27017/we-love-camps");
 mongoose.connection.on("error", console.error.bind(console, "connection error:"));
@@ -49,14 +52,27 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
 	res.locals.success = req.flash("success");
 	res.locals.error = req.flash("error");
 	next();
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+// app.get("/fakeUser", async (req, res) => {
+// 	const user = new User({ email: "example@gmail.com", username: "example" });
+// 	const newUser = await User.register(user, "pswexample");
+// 	res.send(newUser);
+// });
+
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/reviews", reviewsRoutes);
+app.use("", usersRoutes);
 
 app.get("/", (req, res) => {
 	res.render("home");
@@ -72,6 +88,6 @@ app.use((err, req, res) => {
 	res.status(statusCode).render("error", { err });
 });
 
-app.listen(port, () => {
-	console.log(`Serving on port ${port}!`);
+app.listen(3000, () => {
+	console.log("Serving on port 3000!");
 });
