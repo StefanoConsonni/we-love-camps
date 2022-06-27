13321cb4f09257c23ce2664e0261f99de170f960
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-27 11:45:16 +0200

    add review permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
-const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn } = require("../middleware");
 
 router.post(
 	"/",
+	isLoggedIn,
 	validateReview,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		const review = new Review(req.body.review);
+		review.author = req.user._id;
 		campground.reviews.push(review);
 		await review.save();
 		await campground.save();
