9a802425d92ab765970521ee480dc151c60edc8c
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-30 17:49:51 +0200

    edit cloudinary/index.js file

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
 	cloudinary,
-	folder: "WeLoveCamps",
-	allowedFormats: ["jpeg", "png", "jpg"],
+	params: {
+		folder: "WeLoveCamps",
+		allowedFormats: ["jpeg", "png", "jpg"],
+	},
 });
 
 module.exports = {
