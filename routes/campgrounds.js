53a7aacf77b5b9e018ecb64da594869460b70cd7
Stefano Consonni <stefanoconsonni@ymail.com>
2022-06-30 16:30:06 +0200

    install multer and start image upload feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { validateCampground, isLoggedIn, isAuthor } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-router.route("/").get(catchAsync(campgrounds.index)).post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router
+	.route("/")
+	.get(catchAsync(campgrounds.index))
+	// .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+	.post(upload.array("image"), (req, res) => {
+		console.log(req.body, req.files);
+		res.send("IT WORKED!");
+	});
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
