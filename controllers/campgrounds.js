c5675ded9961edc226bea6fbd1df42a4559ea86a
Stefano Consonni <stefanoconsonni@ymail.com>
2022-07-02 18:53:44 +0200

    add deletion of images in backend

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const { cloudinary } = require("../cloudinary/index");
 
 module.exports.index = async (req, res) => {
 	const campgrounds = await Campground.find({});
@@ -50,6 +51,12 @@ module.exports.updateCampground = async (req, res) => {
 	const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
 	campground.images.push(...imgs);
 	await campground.save();
+	if (req.body.deleteImages) {
+		for (const filename of req.body.deleteImages) {
+			await cloudinary.uploader.destroy(filename);
+		}
+		await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+	}
 	req.flash("success", "Successfully updated Campground");
 	res.redirect(`/campgrounds/${campground._id}`);
 };
--- schemas.js
+++ schemas.js
@@ -8,6 +8,7 @@ module.exports.campgroundSchema = Joi.object({
 		location: Joi.string().required(),
 		description: Joi.string().required(),
 	}).required(),
+	deleteImages: Joi.array(),
 });
 
 module.exports.reviewSchema = Joi.object({
