352cc6f220b11e869ad1131f1b11789d86eba9df
Stefano Consonni <stefanoconsonni@ymail.com>
2022-07-10 21:35:37 +0200

    customize map and add map-popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,14 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: "map", // container ID
-	style: "mapbox://styles/mapbox/streets-v11", // style URL
+	style: "mapbox://styles/mapbox/outdoors-v11", // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
-	zoom: 8, // starting zoom
+	zoom: 6, // starting zoom
 	projection: "globe", // display the map as a 3D globe
 });
 map.on("style.load", () => {
 	map.setFog({}); // Set the default atmosphere style
 });
 
-// Create a new marker on the map
-const marker = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+// Create a marker on the map
+const marker = new mapboxgl.Marker()
+	.setLngLat(campground.geometry.coordinates)
+	.setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h6>${campground.title}</h6><p>${campground.location}`))
+	.addTo(map);
