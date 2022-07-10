900debc80444daab915358eed029454f151fbeda
Stefano Consonni <stefanoconsonni@ymail.com>
2022-07-10 17:39:18 +0200

    add centering of the map in show.ejs

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,10 +2,13 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: "map", // container ID
 	style: "mapbox://styles/mapbox/streets-v11", // style URL
-	center: [-74.5, 40], // starting position [lng, lat]
-	zoom: 5, // starting zoom
+	center: campground.geometry.coordinates, // starting position [lng, lat]
+	zoom: 8, // starting zoom
 	projection: "globe", // display the map as a 3D globe
 });
 map.on("style.load", () => {
 	map.setFog({}); // Set the default atmosphere style
 });
+
+// Create a new marker on the map
+const marker = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
